c7d43e877c1e3936a14c13e2fd4ac8031cef2ea8
bettyspurgeon <betty.spurgeon713@gmail.com>
2023-03-05 15:23:39 +0100

    Information for categories

--- information/categories.jsx
+++ information/categories.jsx
@@ -1,7 +1,32 @@
 export const categories = [
-  {title: "K-Pop", percentage: 20, backgroundColor: "#FFD166"},
-  {title: "Pop", percentage: 25, backgroundColor: "#06D6A0"},
-  {title: "Rap", percentage: 15, backgroundColor: "#118AB2"},
-  {title: "Alternative", percentage: 15, backgroundColor: "#073B4C"},
-  {title:"Taylor Swift", percentage: 25, backgroundColor: "#EF476F"} ,
+  {
+    title: "K-Pop",
+    percentage: 20,
+    backgroundColor: "#FFD166",
+    groupping: true,
+  },
+  {
+    title: "Pop",
+    percentage: 25,
+    backgroundColor: "#06D6A0",
+    groupping: false,
+  },
+  {
+    title: "Rap",
+    percentage: 15,
+    backgroundColor: "#118AB2",
+    groupping: false,
+  },
+  {
+    title: "Alternative",
+    percentage: 15,
+    backgroundColor: "#073B4C",
+    groupping: false,
+  },
+  {
+    title: "Taylor Swift",
+    percentage: 25,
+    backgroundColor: "#EF476F",
+    groupping: false,
+  },
 ];
